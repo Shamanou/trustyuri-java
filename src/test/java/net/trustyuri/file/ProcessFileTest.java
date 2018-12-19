ef232209df2c6a0db944fb24e4eb1071f67a486d
shamanou <mandarijnopw8@gmail.com>
2018-12-19 12:30:26 +0100

    code cleanup

--- src/test/java/net/trustyuri/file/ProcessFileTest.java
+++ src/test/java/net/trustyuri/file/ProcessFileTest.java
@@ -1,43 +1,41 @@
 package net.trustyuri.file;
 
-import java.io.File;
-
 import net.trustyuri.CheckFile;
 import net.trustyuri.TrustyUriUtils;
-import net.trustyuri.file.ProcessFile;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+
 
 public class ProcessFileTest {
 
-	@Rule
-	public TemporaryFolder testDir = new TemporaryFolder();
- 
-	@Test
-	public void runTest() throws Exception {
-		File testSuiteDir = new File("src/main/resources/testsuite/FA/valid/");
-		if (testSuiteDir.isDirectory()) {
-			for (File testFile : testSuiteDir.listFiles()) {
-				String name = testFile.getName();
-				if (!TrustyUriUtils.isPotentialTrustyUri(name)) continue;
-				String preName = name.replaceFirst("^(.*)\\.(FA[A-Za-z0-9\\-_]{43})(\\.[a-z]+)", "$1$3");
-				test(name, preName);
-			}
-		}
-	}
+    @Rule
+    public TemporaryFolder testDir = new TemporaryFolder();
 
-	public void test(String name, String preName) throws Exception {
-		File preFile = new File(testDir.getRoot(), preName);
-		FileUtils.copyFile(new File("src/main/resources/testsuite/FA/valid/" + name), preFile);
-		ProcessFile.main(new String[] {preFile.getAbsolutePath()});
-		File file = new File(testDir.getRoot(), name);
-		assert !preFile.exists();
-		assert file.exists();
-		CheckFile.main(new String[] {file.getAbsolutePath()});
-	}
+    @Test
+    public void runTest() throws Exception {
+        File testSuiteDir = new File("src/main/resources/testsuite/FA/valid/");
+        if (testSuiteDir.isDirectory()) {
+            for (File testFile : testSuiteDir.listFiles()) {
+                String name = testFile.getName();
+                if (!TrustyUriUtils.isPotentialTrustyUri(name)) continue;
+                String preName = name.replaceFirst("^(.*)\\.(FA[A-Za-z0-9\\-_]{43})(\\.[a-z]+)", "$1$3");
+                test(name, preName);
+            }
+        }
+    }
+
+    public void test(String name, String preName) throws Exception {
+        File preFile = new File(testDir.getRoot(), preName);
+        FileUtils.copyFile(new File("src/main/resources/testsuite/FA/valid/" + name), preFile);
+        ProcessFile.main(new String[]{preFile.getAbsolutePath()});
+        File file = new File(testDir.getRoot(), name);
+        assert !preFile.exists();
+        assert file.exists();
+        CheckFile.main(new String[]{file.getAbsolutePath()});
+    }
 
 }
--- src/test/java/net/trustyuri/rdf/CheckLargeRdfTest.java
+++ src/test/java/net/trustyuri/rdf/CheckLargeRdfTest.java
@@ -1,33 +1,31 @@
 package net.trustyuri.rdf;
 
-import java.io.File;
-
-import net.trustyuri.rdf.CheckLargeRdf;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+
 public class CheckLargeRdfTest {
 
-	@Rule
-	public TemporaryFolder testDir = new TemporaryFolder();
- 
-	@Test
-	public void runTest() throws Exception {
-		File testSuiteDir = new File("src/main/resources/testsuite/RA/valid/");
-		if (testSuiteDir.isDirectory()) {
-			for (File testFile : testSuiteDir.listFiles()) {
-				test(testFile.getName());
-			}
-		}
-	}
+    @Rule
+    public TemporaryFolder testDir = new TemporaryFolder();
 
-	public void test(String filename) throws Exception {
-		File file = new File("src/main/resources/testsuite/RA/valid/" + filename);
-		CheckLargeRdf c = new CheckLargeRdf(file);
-		boolean valid = c.check();
-		assert valid;
-	}
+    @Test
+    public void runTest() throws Exception {
+        File testSuiteDir = new File("src/main/resources/testsuite/RA/valid/");
+        if (testSuiteDir.isDirectory()) {
+            for (File testFile : testSuiteDir.listFiles()) {
+                test(testFile.getName());
+            }
+        }
+    }
+
+    public void test(String filename) throws Exception {
+        File file = new File("src/main/resources/testsuite/RA/valid/" + filename);
+        CheckLargeRdf c = new CheckLargeRdf(file);
+        boolean valid = c.check();
+        assert valid;
+    }
 
 }
